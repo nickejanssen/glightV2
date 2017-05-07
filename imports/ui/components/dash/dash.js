5cdfb8f2ec6280e6caf0cd0681b34f1ec4479340
nickejanssen <nickejanssen@gmail.com>
2017-05-06 20:54:19 -0400

    Update UI for login and registration forms

--- imports/ui/components/dash/dash.js
+++ imports/ui/components/dash/dash.js
@@ -2,11 +2,15 @@ import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tracker } from 'meteor/tracker';
-import { Company } from '../../../api/company/company.js';
+import { moment } from "meteor/momentjs:moment";
 import './dash.html';
 
 Template.dash.helpers({
-
+  LoginUserName() {
+    let _user = Meteor.user();
+    if (_.isObject(_user))
+      return _user.profile.fname + " " + _user.profile.lname;
+  }
 });
 
 Template.companyTable.helpers({
@@ -19,8 +23,10 @@ Template.dash.events({
 });
 
 Template.dash.onCreated(function () {
+
 });
 
 Template.companyTable.onRendered(function(){
-initDataTableComponents();
+  initDataTableComponents();
+  $('body').addClass('has-detached-left');
 });
